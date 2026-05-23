821ddf4ddbbee0f59bbc5172db5c95bcdea31d64
dibyansu2611 <dibyansu.mahapatra@rumango.com>
2026-05-23 19:55:10 +0530

    Modifying to Test Backend API

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,16 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
+  root: ".", // <-- make sure root is project root where index.html lives
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    outDir: "dist", // default, ensures build output goes to /app/dist
+    emptyOutDir: true, // cleans dist before build
   },
   server: {
     port: 8081,
